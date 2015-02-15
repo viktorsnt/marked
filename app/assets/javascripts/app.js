3251919505fdb91007709691d4c00dc3961277a8
Victor Almeida <viktorsnt@gmail.com>
2015-02-15 13:51:20 -0200

    login and register

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,4 +1,4 @@
-var app = angular.module("flapperNews", ['ui.router', 'templates']);
+var app = angular.module("flapperNews", ['ui.router', 'templates', 'Devise']);
 
 app.config(['$stateProvider', '$urlRouterProvider', '$httpProvider',
 	function($stateProvider, $urlRouterProvider, $httpProvider) {
@@ -25,6 +25,26 @@ app.config(['$stateProvider', '$urlRouterProvider', '$httpProvider',
 				    return posts.get($stateParams.id);
 				  }]
 	    	}
+	    })
+	    .state('login', {
+	    	url: '/login',
+	    	templateUrl: 'auth/_login.html',
+	    	controller: 'AuthCtrl',
+	      onEnter: ['$state', 'Auth', function($state, Auth) {
+	        Auth.currentUser().then(function (){
+	          $state.go('home');
+	        })
+	      }]
+	    })
+	    .state('register', {
+	    	url: '/register',
+	    	templateUrl: 'auth/_register.html',
+	    	controller: 'AuthCtrl',
+	      onEnter: ['$state', 'Auth', function($state, Auth) {
+	        Auth.currentUser().then(function (){
+	          $state.go('home');
+	        })
+	      }]
 	    });
 
   	$urlRouterProvider.otherwise('home');
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,18 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+	before_action :configure_permitted_parameters, if: :devise_controller?
+  
   respond_to :json
 
   def angular
     render 'layouts/application'
+  end
+  
+  private
+  
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :username
   end
 
 end
