4f1be45051849287cdad5213d5abbeca41a3249e
Victor Almeida <viktorsnt@gmail.com>
2015-02-15 14:16:12 -0200

    fix

--- app/assets/javascripts/mainCtrl.js
+++ app/assets/javascripts/mainCtrl.js
@@ -1,15 +1,18 @@
-app.controller('MainCtrl', ['$scope', 'posts', function($scope, posts) {
-    $scope.posts = posts.posts;
-    $scope.addPost = function(){
-    	if(!$scope.title || $scope.title === '') { return; }
-    	posts.create({
-    		title: $scope.title,
-    		link: $scope.link
-  		});
-    	$scope.title = '';
-    	$scope.link = '';
-    };
-    $scope.incrementUpvote = function(post){
-    	posts.upvote(post);
-    };
-  }]);
+app.controller('MainCtrl', ['$scope', 'posts', 'Auth', function($scope, posts, Auth) {
+  $scope.signedIn = Auth.isAuthenticated;
+  $scope.posts = posts.posts;
+  $scope.addPost = function() {
+    if (!$scope.title || $scope.title === '') {
+      return;
+    }
+    posts.create({
+      title: $scope.title,
+      link: $scope.link
+    });
+    $scope.title = '';
+    $scope.link = '';
+  };
+  $scope.incrementUpvote = function(post) {
+    posts.upvote(post);
+  };
+}]);
